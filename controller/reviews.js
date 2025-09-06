c3cc8938ba9f936f693eef95539cb6326c5d325a
nitin7707 <nitinshrivastav191@gmail.com>
2025-09-06 15:36:34 +0530

    fix: use process.env.PORT for Render

--- controller/reviews.js
+++ controller/reviews.js
@@ -2,14 +2,25 @@ const Review = require('../models/review.js');
 const Listing = require('../models/listing.js');
 
 module.exports.createReview = async (req, res) => {
-    let foundListing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    foundListing.reviews.push(newReview);
-    await newReview.save();
-    await foundListing.save();
-    req.flash('success','New review Created');
-    res.redirect(`/listings/${foundListing._id}`);
+    try {
+        let foundListing = await Listing.findById(req.params.id);
+        if (!foundListing) {
+            req.flash('error', 'Listing not found');
+            return res.redirect('/listings');
+        }
+        
+        let newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+        foundListing.reviews.push(newReview);
+        await newReview.save();
+        await foundListing.save();
+        req.flash('success','New review Created');
+        res.redirect(`/listings/${foundListing._id}`);
+    } catch (error) {
+        console.error('Error creating review:', error);
+        req.flash('error', 'Failed to create review');
+        res.redirect(`/listings/${req.params.id}`);
+    }
 }
 
 module.exports.deleteReview = async (req, res, next) => {
